4cc3464e220ee452e089ddad48e722712d54ece1
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-03-09 16:47:41 -0300

    Mais mudanças

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const fs = require('fs');
-const { findPackageJSON } = require('module');
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    if (erro) { 
-    console.log('qual é o erro?', erro.code);
-        return
+    try {
+        if (erro) throw erro;
+        contarPalavras(texto);
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log('Arquivo não encontrado');
+        } else {
+            console.log('Ocorreu outro erro');
+        }
     }
 });
 
 function contarPalavras(texto) {
-    const paragrafos = extraiParagrafos(texto)
+    const paragrafos = extraiParagrafos(texto);
+
     const contagem = paragrafos.flatMap((paragrafo) => {
         if (!paragrafo) return [];
-            return verificarPalavrasDuplicadas(paragrafo); 
-    })
+        return [verificarPalavrasDuplicadas(paragrafo)];
+    });
+
     console.log(contagem);
 }
 
@@ -25,18 +32,19 @@ function extraiParagrafos(texto) {
 }
 
 function limpaPalavras(palavra) {
-  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 }
 
 function verificarPalavrasDuplicadas(texto) {
-    const listaPalavras = texto.split(/\s+/); // separa por qualquer espaço
-    const resultado = {}; // objeto correto
+    const listaPalavras = texto.split(/\s+/);
+    const resultado = {};
+
     listaPalavras.forEach((palavra) => {
         if (palavra.length >= 3) {
-        const palavraLimpa = limpaPalavras(palavra);
-        resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+            const palavraLimpa = limpaPalavras(palavra);
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
         }
-    })
+    });
+
     return resultado;
 }
-5

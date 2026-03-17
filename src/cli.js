0622064744aa547dd4dfdf37b39e58bc4aceb784
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-03-17 11:58:44 -0300

    Implementando lbls externas

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 import { contarPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
 import { Command } from 'commander';
 
 const program = new Command();
+
 program
     .version('0.0.1')
     .option('-t, --texto <string>', 'caminho do texto a ser processado')
@@ -13,29 +15,42 @@ program
         const { texto, destino } = options;
 
         if (!texto || !destino) {
-            console.error('erro: favor inserir caminho de origem e destino')
+            console.error('erro: favor inserir caminho de origem e destino');
             program.help();
             return;
         }
-    })
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    try {
-        if (erro) throw erro;
-        const resultado =contarPalavras(texto);
-        criaESalvaArquivo(resultado, endereco);
-    } catch (erro) {
-       trataErros(erro);
-    }
-});
+        try {       
+            processaArquivo(caminhoTexto, caminhoDestino);   
+            console.log('texto processado com sucesso');  
+        } catch (erro) {
+            console.log('ocorreu um erro no processamento', erro);
+        }
+    });
+
+program.parse();
+
+function processaArquivo(caminhoTexto, endereco) {
+    fs.readFile(caminhoTexto, 'utf-8', (erro, texto) => {
+        try {
+            if (erro) throw erro;
+
+            const resultado = contarPalavras(texto);
+            criaESalvaArquivo(resultado, endereco);
+
+        } catch (erro) {
+            trataErros(erro);
+        }
+    });
+}
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
-    const arquivoNovo = `${endereco}/resultado.txt`;
+    const arquivoNovo = path.join(endereco, 'resultado.txt');
     const textoPalavras = montaSaidaArquivo(listaPalavras);
+
     try {
         await fs.promises.writeFile(arquivoNovo, textoPalavras);
         console.log('Arquivo criado');
